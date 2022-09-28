2103247f15f6f0bb1a79dd5e43ce1ec50dc7b18b
MB <mb@blaster.ai>
2022-09-28 22:11:16 +0300

    Most of schema

--- src/telegram/schema.clj
+++ src/telegram/schema.clj
@@ -17,6 +17,69 @@
    :e/some-string [:string {:min 1}]
 
    ;; API Objects
+   :api/File
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:file-size {:optional true} pos-int?]
+    [:file-path {:optional true} :e/some-string]]
+
+   :api/Animation
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:width pos-int?]
+    [:height pos-int?]
+    [:duration pos-int?]
+    [:thumb {:optional true} :api/PhotoSize]
+    [:file-name {:optional true} :e/some-string]
+    [:mime-type {:optional true} :e/some-string]
+    [:file-size {:optional true} pos-int?]]
+
+   :api/Audio
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:duration pos-int?]
+    [:performer {:optional true} :e/some-string]
+    [:title {:optional true} :e/some-string]
+    [:mime-type {:optional true} :e/some-string]
+    [:file-size {:optional true} pos-int?]
+    [:thumb {:optional true} :api/PhotoSize]]
+
+   :api/Document
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:thumb {:optional true} :api/PhotoSize]
+    [:file-name {:optional true} :e/some-string]
+    [:mime-type {:optional true} :e/some-string]
+    [:file-size {:optional true} pos-int?]]
+
+   :api/Sticker
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:type [:enum "regular" "mask" "custom_emoji"]]
+    [:width pos-int?]
+    [:height pos-int?]
+    [:is-animated :boolean]
+    [:is-video :boolean]
+    [:thumb {:optional true} :api/PhotoSize]
+    [:emoji {:optional true} :e/some-string]
+    [:set-name {:optional true} :e/some-string]
+    [:premium-animation {:optional true} :api/File]
+    [:mask-position {:optional true} :api/MaskPosition]
+    [:custom-emoji-id {:optional true} :e/some-string]
+    [:file-size {:optional true} pos-int?]]
+
+   :api/MaskPosition
+   [:map
+    [:point :e/some-string]
+    [:x-shift float?]
+    [:y-shift float?]
+    [:scale float?]]
+
    :api/Update
    [:map
     [:update-id :e/update-id]
@@ -26,14 +89,24 @@
     [:edited-channel-post {:optional true} [:ref :api/Message]]
     [:inline-query {:optional true} :InlineQuery]
     [:chosen-inline-result {:optional true} :ChosenInlineResult]
-    [:callback-query {:optional true} :CallbackQuery]
+    [:callback-query {:optional true} :api/CallbackQuery]
     [:shipping-query {:optional true} :ShippingQuery]
-    [:pre_checkout_query {:optional true} :PreCheckoutQuery]
+    [:pre-checkout-query {:optional true} :PreCheckoutQuery]
     [:poll {:optional true} :Poll]
     [:poll-answer {:optional true} :PollAnswer]
     [:my-chat-member {:optional true} :ChatMemberUpdated]
     [:chat-member {:optional true} :ChatMemberUpdated]
     [:chat-join-request {:optional true} :ChatJoinRequest]]
+
+   :api/CallbackQuery
+   [:map
+    [:id :e/some-string]
+    [:from :api/User]
+    [:message {:optional true} :api/Message]
+    [:inline-message-id {:optional true} :e/some-string]
+    [:chat-instance :e/some-string]
+    [:data {:optional true} :e/some-string]
+    [:game-short-name {:optional true} :e/some-string]]
 
    :api/User
    [:map
@@ -108,11 +181,11 @@
     [:author-signature {:optional true} :e/some-string]
     [:text {:optional true} :e/some-string]
     [:entities {:optional true} [:vector :api/MessageEntity]]
-    [:animation {:optional true} :Animation]
-    [:audio {:optional true} :Audio]
-    [:document {:optional true} :Document]
-    [:photo {:optional true} [:vector :PhotoSize]]
-    [:sticker {:optional true} :Sticker]
+    [:animation {:optional true} :api/Animation]
+    [:audio {:optional true} :api/Audio]
+    [:document {:optional true} :api/Document]
+    [:photo {:optional true} [:vector :api/PhotoSize]]
+    [:sticker {:optional true} :api/Sticker]
     [:video {:optional true} :Video]
     [:video-note {:optional true} :VideoNote]
     [:voice {:optional true} :Voice]
@@ -123,11 +196,11 @@
     [:game {:optional true} :Game]
     [:poll {:optional true} :Poll]
     [:venue {:optional true} :Venue]
-    [:location {:optional true} :Location]
+    [:location {:optional true} :api/Location]
     [:new-chat-members {:optional true} [:vector :api/User]]
     [:left-chat-member {:optional true} [:ref :api/User]]
     [:new-chat-title {:optional true} :e/some-string]
-    [:new-chat-photo {:optional true} [:vector :PhotoSize]]
+    [:new-chat-photo {:optional true} [:vector :api/PhotoSize]]
     [:delete-chat-photot {:optional true} :boolean]
     [:group-chat-created {:optional true} :boolean]
     [:supergeroup-chat-created {:optional true} :boolean]
@@ -196,16 +269,12 @@
     [:location :api/Location]
     [:address [:string {:min 1}]]]
 
-   :WebAppInfo :string                  ;TODO
-
    :LoginURL
    [:map
     [:url :e/some-string]
     [:forward-text {:optional true} :e/some-string]
     [:bot-username {:optional true} :e/some-string]
     [:request-write-access {:optional true} :boolean]]
-
-   :CallbackGame :string                ;TODO
 
    :InlineKeyboardButton
    [:map
@@ -219,6 +288,44 @@
     [:callback-game {:optional true} :CallbackGame]
     [:pay {:optional true} :boolean]]
 
+   :api/PhotoSize
+   [:map
+    [:file-id :e/some-string]
+    [:file-unique-id :e/some-string]
+    [:width pos-int?]
+    [:height pos-int?]
+    [:file-size {:optional true} pos-int?]]
+
+   ;; TODO: implement these classes
+   :not-implemented :boolean
+   :WebAppInfo :not-implemented
+   :CallbackGame :not-implemented
+   :Video :not-implemented
+   :VideoNote :not-implemented
+   :Voice :not-implemented
+   :Contact :not-implemented
+   :Dice :not-implemented
+   :Game :not-implemented
+   :Poll :not-implemented
+   :Venue :not-implemented
+   :AutoDeleteTimerChanged :not-implemented
+   :Invoice :not-implemented
+   :SuccessfulPayment :not-implemented
+   :PassportData :not-implemented
+   :ProximityAlertTriggered :not-implemented
+   :VideoChatScheduled :not-implemented
+   :VideoChatStarted :not-implemented
+   :VideoChatEnded :not-implemented
+   :VideoChatParticipantsInvited :not-implemented
+   :WebAppData :not-implemented
+   :InlineQuery :not-implemented
+   :ChosenInlineResult :not-implemented
+   :ShippingQuery :not-implemented
+   :PreCheckoutQuery :not-implemented
+   :PollAnswer :not-implemented
+   :ChatMemberUpdated :not-implemented
+   :ChatJoinRequest :not-implemented
+
    ;; Used for recursion
    :inline-button-row [:vector :InlineKeyboardButton]
    :inline-button-rows [:vector [:alt :inline-button-row :InlineKeyboardButton]]
@@ -228,17 +335,12 @@
    })
 
 
-(defn get-schema [identifier]
+(defn get-schema
+  "Get schema from the `TelegramRegistry` by keyword identifier."
+  [identifier]
   (m/schema
    [:schema
     {:registry TelegramRegistry}
     identifier]))
 
 (def schema:User (get-schema :api/User))
-
-(def schema:InlineKeyboardMarkup
-  (m/schema
-   [:schema
-    {:registry TelegramRegistry}
-    :InlineKeyboardMarkup]))
-
